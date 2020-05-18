78edbd17c61066c51193c8c25630839a6c537fa3
eleger <emmanuel.leger@u-psud.fr>
2020-05-18 13:07:47 +0200

    fin reformating

--- FormatOUT_Old2New.py
+++ FormatOUT_Old2New.py
@@ -33,7 +33,7 @@ fname=next(os.walk(data_path))[1]
 #%%
 
 lst=[]
-blib=pd.DataFrame(columns=['TWT(ns)'])
+blib=pd.DataFrame(columns=['TWT(ns)','Volumes'])
 for ii in fname:
         
     filename = 'TWT'
@@ -50,7 +50,27 @@ for ii in fname:
                     twts[i] = float(mot)
                     i=i+1
         fTWT.close()
+        twts=insert(twts,0,0)#jte jure ...
         blib['TWT(ns)']=twts
-        blib['TWT(ns)'].to_csv('./OUTdtrou30_rtrou4_tr5.0/OUTdtrou30_rtrou4_tr5.0/'+ii+'/TWT_EL.csv', sep=',', encoding='utf-8',index=False,header='TWT(ns)')
-        #input(2)
+        blib['TWT(ns)'].to_csv('./OUTdtrou30_rtrou4_tr5.0/OUTdtrou30_rtrou4_tr5.0/'+ii+'/TWT_EL.csv', sep=',', encoding='utf-8',index=None,header=True)
+     
+    filename = 'Volumes'
+    if (os.path.isfile('./OUTdtrou30_rtrou4_tr5.0/OUTdtrou30_rtrou4_tr5.0/'+ii+"/"+filename)):   
+        volumes=np.zeros(11)
+        fVOL=open('./OUTdtrou30_rtrou4_tr5.0/OUTdtrou30_rtrou4_tr5.0/'+ii+"/Volumes","r")
+        i=0
+        for ligne in fVOL:
+              ligne = ligne.rstrip(']\n')
+              mots = ligne.split(" ")
+              for mot in mots:
+                  if (mot != '' and mot != '[0.' and mot != '[' and mot != '0.'):
+                      volumes[i] = float(mot)
+                      i=i+1
+        fVOL.close()  
+        volumes=insert(volumes,0,0)
+        blib['Volumes']=volumes
+        blib['Volumes'].to_csv('./OUTdtrou30_rtrou4_tr5.0/OUTdtrou30_rtrou4_tr5.0/'+ii+'/Volumes_EL.csv', sep=',', encoding='utf-8',index=None,header=True)
+        
+        
+
         
