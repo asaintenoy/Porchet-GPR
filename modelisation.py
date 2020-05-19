a70b951b94fa8c4dfafc62c6009c1a1d7abdafc5
eleger <emmanuel.leger@u-psud.fr>
2020-05-19 10:47:40 +0200

    merging modif modelisations

--- modelisation.py
+++ modelisation.py
@@ -34,28 +34,28 @@ with warnings.catch_warnings():
     fxn()
 
 #Def des paramètres de géométrie du modéle
-class Geometry :
-    def __repr__(self):
-        return "dtrou{dtrou}_rtrou{r}_tr{h_eau}".format(dtrou=self.dtrou,r=self.r,h_eau=self.h_eau)        
+# class Geometry :
+#     def __repr__(self):
+#         return "dtrou{dtrou}_rtrou{r}_tr{h_eau}".format(dtrou=self.dtrou,r=self.r,h_eau=self.h_eau)        
 
-# Definition des paramètres MVG
-class ParamMVG(namedtuple("ParamMVG", ["ts", "ti", "tr", "n", "alpha", "Ks"])):
+# # Definition des paramètres MVG
+# class ParamMVG(namedtuple("ParamMVG", ["ts", "ti", "tr", "n", "alpha", "Ks"])):
                
-    #Recalcul des h0 à partir des param MVG
-    def h0(self):
-        return -(1/self.alpha)*(((self.ti-self.tr)/(self.ts-self.tr))**(self.n/(1-self.n))-1)**(1/self.n)
-    def __repr__(self):
-        return "ts{ts}_ti{ti}_tr{tr}_n{n}_alpha{alpha}_Ks{Ks}".format(ts=self.ts,ti=self.ti,tr=self.tr,n=self.n,alpha=self.alpha,Ks=self.Ks)        
+#     #Recalcul des h0 à partir des param MVG
+#     def h0(self):
+#         return -(1/self.alpha)*(((self.ti-self.tr)/(self.ts-self.tr))**(self.n/(1-self.n))-1)**(1/self.n)
+#     def __repr__(self):
+#         return "ts{ts}_ti{ti}_tr{tr}_n{n}_alpha{alpha}_Ks{Ks}".format(ts=self.ts,ti=self.ti,tr=self.tr,n=self.n,alpha=self.alpha,Ks=self.Ks)        
 
-# Definition des param gprMax
-class ParamGPRMAX :
-    pass
+# # Definition des param gprMax
+# class ParamGPRMAX :
+#     pass
 
-def longueur_d_onde(theta, freq, paramMVG, paramGPRMAX):
-    """Compute the wavelength for a given water content and frequency (Hz)"""
-    eps = CRIM(theta,paramMVG,paramGPRMAX)
-    vitesse = 0.3 / math.sqrt(eps) # m/ns
-    return vitesse*(10**9)/freq
+# def longueur_d_onde(theta, freq, paramMVG, paramGPRMAX):
+#     """Compute the wavelength for a given water content and frequency (Hz)"""
+#     eps = CRIM(theta,paramMVG,paramGPRMAX)
+#     vitesse = 0.3 / math.sqrt(eps) # m/ns
+#     return vitesse*(10**9)/freq
 
 def run(geometry,paramMVG,paramGPRMAX,temps,tmax_SWMS2D):
     # try: c'est pour pouvoir prendre la main si ça foire.
