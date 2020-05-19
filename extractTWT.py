462f6f8ae65062fb37b280b387509c3614325d50
Albane Saintenoy <albane.saintenoy@u-psud.fr>
2020-05-19 11:28:47 +0200

    extract TWT nettoyé

--- extractTWT.py
+++ extractTWT.py
@@ -32,38 +32,34 @@ dirName = "OUTtest"
 for element in os.listdir(dirName):
     if os.path.isfile(element):
         print("'%s' pas un dossier" % element)
-    else:
-        ####### Parameters ######
-#        filename = 'Parameters'
-#        s = io.open(dirName + "/"+ element + "/" + filename).read()    
-#        p = eval(s)
-        p = read_parameters(dirName + "/" + element + "/")
-        print(p)
-        print(p[1])
+        continue
     
-        if p[1] == p[1] : #p[1] est ti
-            print(p)#Calcul des TWT
-            # Definition des paramètres MVG
-            paramMVG = ParamMVG(tr=p[2], ts=p[0], ti=p[1], Ks=p[5], n=p[3], alpha=p[4])
-            paramMVG.porosity = paramMVG.ts
-            
-            filename_rada = 'radargram__merged.out'
-            
-            if (not os.path.isfile(dirName + "/" + element + 'TWT')) :
-                print("Pas de TWT dans " + dirName + "/" + element)
-                if os.path.isfile(dirName + "/" + element + '/'+ filename_rada) :
-                    filename_path =  dirName + "/" + element + '/' + filename_rada
-                                
-                    print("Picking radargramme dans " + dirName + "/" + element)
-                    cas,dt,itmin0,ifenetre,tps_min1,tps_min1_0,tps_min2,tps_min2_0,tps_max,tps_max0,TWT \
-                        = picking(filename_path, nT, geometry, paramMVG, paramGPRMAX, temps)  
-                    rada_plot(dirName + "/" + element + "/")    
-                    # plot des points piqués
-                    axe=np.arange(12)
-                    plt.plot(axe, (tps_max+tps_max0)/dt,'bo', label='max')        
-                    plt.plot(axe, (tps_min1+tps_min1_0)/dt,'ro', label='min1')  
-                    plt.plot(axe, (tps_min2+tps_min2_0)/dt,'go', label='min2') 
-                    plt.legend(fontsize=20)
-                        
+    p = read_parameters(dirName + "/" + element + "/")
+    paramMVG = ParamMVG(tr=p[2], ts=p[0], ti=p[1], Ks=p[5], n=p[3], alpha=p[4])
+    paramMVG.porosity = paramMVG.ts
+    
+    filename_rada = 'radargram__merged.out'
+    
+    if (os.path.isfile(dirName + "/" + element + 'TWT')) :
+        continue
+    print("Pas de TWT dans " + dirName + "/" + element)
+    
+    filename_path =  dirName + "/" + element + '/' + filename_rada
+    
+    if not os.path.isfile(filename_path) :
+        print("pas de radargramme dans " + dirName + "/" + element)
+        continue
+    print("Picking radargramme dans " + dirName + "/" + element)
+    cas,dt,itmin0,ifenetre,tps_min1,tps_min1_0,tps_min2,tps_min2_0,tps_max,tps_max0,TWT \
+        = picking(filename_path, nT, geometry, paramMVG, paramGPRMAX, temps)  
+    
+    rada_plot(dirName + "/" + element + "/")    
+    # plot des points piqués
+    axe=np.arange(12)
+    plt.plot(axe, (tps_max+tps_max0)/dt,'bo', label='max')        
+    plt.plot(axe, (tps_min1+tps_min1_0)/dt,'ro', label='min1')  
+    plt.plot(axe, (tps_min2+tps_min2_0)/dt,'go', label='min2') 
+    plt.legend(fontsize=20)
+                    
                         
                         
