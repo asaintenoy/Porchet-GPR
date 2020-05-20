7d08a7e60a5586a477419a91002514e033f0be8a
eleger <emmanuel.leger@u-psud.fr>
2020-05-20 11:38:45 +0200

    process

--- SimuResultProcs_V1.py
+++ SimuResultProcs_V1.py
@@ -54,34 +54,65 @@ for ii in fname:
     except:
         bibi=1
     
-    lst.append([paramMVG.tr,paramMVG.ts,paramMVG.n,paramMVG.alpha,paramMVG.Ks,rmse,bibi])
+    lst.append([paramMVG.tr,paramMVG.ts,paramMVG.ti,paramMVG.n,paramMVG.alpha,paramMVG.Ks,rmse,bibi])
     
-df_params=pd.DataFrame(lst,columns=['tr','ts','n','alpha','Ks','RMSE','Converged'])                 
+df_params=pd.DataFrame(lst,columns=['tr','ts','ti','n','alpha','Ks','RMSE','Converged'])                 
 
 
 #%% 
 plt.close('all')
-legendounet=['tr','ts','n','alpha','Ks']
+df_params=pd.DataFrame(lst,columns=['tr','ts','ti','n','alpha','Ks','RMSE','Converged'])
+plt.close('all')
+legendounet=['ti','ts','n','alpha','Ks']
+df_params=df_params[(df_params['tr']==0.03) & (df_params['Ks']<0.49) & (df_params['n']<10.1) ]
+
 (f1, ax)= plt.subplots(5,5,figsize=(25,15))
-ax[0,0].scatter(df_params.tr,df_params.ts,c=df_params.RMSE)
-ax[0,0].grid()
-ax[0,0].set_xlabel(legendounet[0])
-ax[0,0].set_xlabel(legendounet[1])
-ax[0,1].scatter(df_params.tr,df_params.n,c=df_params.RMSE)
-ax[0,1].grid()
-ax[0,1].set_xlabel(legendounet[0])
-ax[0,1].set_xlabel(legendounet[2])
-ax[1,0].scatter(df_params.tr,df_params.n,c=df_params.RMSE)
-ax[1,0].grid()
-ax[1,1].scatter(df_params.n,df_params.alpha,c=df_params.RMSE)
-ax[1,1].grid()
+for ii in range(5):
+    for jj in range(5):
+        if(ii==jj):
+            ax[ii,jj].hist(df_params[legendounet[ii]], weights=np.zeros_like(df_params[legendounet[ii]]) + 1. / size(df_params[legendounet[ii]]))
+            ax[ii,jj].set_xlabel(legendounet[ii])
+            ax[ii,jj].set_ylabel('Rel Freq.')
+            ax[ii,jj].grid()
+        else:
+            ax[ii,jj].scatter(df_params[legendounet[ii]],df_params[legendounet[jj]],c=df_params.RMSE,cmap = 'jet')
+            ax[ii,jj].grid()
+            ax[ii,jj].set_xlabel(legendounet[ii])
+            ax[ii,jj].set_ylabel(legendounet[jj])
+#f1.tight_layout()
+        #ax[ii,jj].xaxis.set_label_position('top') 
 
 
-
-
-
+left  = 0.045  # the left side of the subplots of the figure
+right = 0.988    # the right side of the subplots of the figure
+bottom = 0.049   # the bottom of the subplots of the figure
+top = 0.987      # the top of the subplots of the figure
+wspace = 0.224   # the amount of width reserved for blank space between subplots
+hspace = 0.290   # the amount of height reserved for white space between subplots
+plt.subplots_adjust(left=left, bottom=bottom, right=right, top=top, wspace=wspace, hspace=hspace)
+plt.colorbar()
+f1.savefig('RMSE.png',format='png')
 #%%
-pd.plotting.scatter_matrix(df_params)
+plt.close('all')
+legendounet=['tr','ts','n','alpha','Ks']
+(f1, ax)= plt.subplots(3,2,figsize=(25,15))
+ax[0,0].scatter(df_params['alpha'],df_params.RMSE)
+ax[0,0].set_xlabel('alpha')
+ax[0,0].grid()
+ax[0,1].scatter(df_params['Ks'],df_params.RMSE)
+ax[0,1].set_xlabel('Ks')
+ax[0,1].grid()
+ax[1,1].scatter(df_params['n'],df_params.RMSE)
+ax[1,1].set_xlabel('n')
+ax[1,1].grid()
+ax[2,1].scatter(df_params['ts'],df_params.RMSE)
+ax[2,1].set_xlabel('ts')
+ax[2,1].grid()
+# for ii in range(3):
+#     for jj in range(2):
+#         ax[ii,jj].scatter(df_params[legendounet[ii]],df_params.RMSE)
+#         ax[ii,jj].set_xlabel(legendounet[ii])
+
 #%% seqborn
 # plt.close('all')
 # f1, ax1= plt.subplots(1,1,figsize=(25,15))
