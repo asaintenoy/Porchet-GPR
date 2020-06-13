54d89e863634a05841c82619d0d5a051ef127363
eleger <emmanuel.leger@u-psud.fr>
2020-06-13 11:59:08 +0200

    ajoutTWT visualisation

--- SimuResultProcs_V2.py
+++ SimuResultProcs_V2.py
@@ -158,18 +158,24 @@ f2.savefig('./plots/Histo_'+str(100*pc)+'pc_'+Nama+'.png',format='png')
 
 
 #%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% Compa Un par un mais en boucle
-hahat=glob.glob('/home/el/Data/Compil_data-Kriterres/061218-Cul-du-chien/Fit-avec-baseOUTdtrou30_rtrou4_tr5.0/twt*.txt')
-hahav=glob.glob('/home/el/Data/Compil_data-Kriterres/061218-Cul-du-chien/Fit-avec-baseOUTdtrou30_rtrou4_tr5.0/vol*.txt')
+#hahat=glob.glob('/home/el/Data/Compil_data-Kriterres/061218-Cul-du-chien/Fit-avec-baseOUTdtrou30_rtrou4_tr5.0/twt*.txt')
+#hahav=glob.glob('/home/el/Data/Compil_data-Kriterres/061218-Cul-du-chien/Fit-avec-baseOUTdtrou30_rtrou4_tr5.0/vol*.txt')
+
+hahat=glob.glob('/home/el/Data/Compil_data-Kriterres/190527-Poligny/Fit-avec-baseOUTdtrou30_rtrou4_tr5.0/TWT*.txt')
+hahav=glob.glob('/home/el/Data/Compil_data-Kriterres/190527-Poligny/Fit-avec-baseOUTdtrou30_rtrou4_tr5.0/Vol*.txt')
+
+
 lst=[]
 #%% Reading the folder names
 fname=next(os.walk('./OUTdtrou30_rtrou4_tr5.0/'))[1]
 
 #%%
-ouca='Bilbo'
+ouca='Poligny'
+fontouney=20
 for filit,filiv in zip(hahat,hahav):
     lst=[]
-    #Nama=filit[-11:-4] #Poligny
-    Nama=filit[-12:-4]#Bilbo
+    Nama=filit[-11:-4] #Poligny
+    #Nama=filit[-12:-4]#Bilbo
     temp=np.genfromtxt(filit, delimiter=' ')
     TWT_XP=temp[:,1]
     Time_TWT_XP=temp[:,0]
@@ -196,22 +202,65 @@ for filit,filiv in zip(hahat,hahav):
             rmsevol=np.nan
             rmse=np.nan
         
-        lst.append([paramMVG.tr,paramMVG.ts,paramMVG.ti,paramMVG.n,paramMVG.alpha,paramMVG.Ks,rmseTwt,rmsevol,rmse,bibi])
+        lst.append([paramMVG.tr,paramMVG.ts,paramMVG.ti,paramMVG.n,paramMVG.alpha,paramMVG.Ks,rmseTwt,rmsevol,rmse,bibi,(temp),(vol)])
         
-    df_params=pd.DataFrame(lst,columns=['tr','ts','ti','n','alpha','Ks','RMSETWT','RMSEVOL','RMSE','Converged']) 
+    df_params=pd.DataFrame(lst,columns=['tr','ts','ti','n','alpha','Ks','RMSETWT','RMSEVOL','RMSE','Converged','TWT','VOL']) 
+    
+    
+    
+################Sensibility plot
+    # plt.close('all')
+    # legendounet=['ti','ts','n','alpha','Ks']
+    # #df_params=df_params[(df_params['tr']==0.03) & (df_params['Ks']<0.49) & (df_params['Ks']>0.07) & (df_params['n']<10.1) ]
+    
+    # (f1, ax)= plt.subplots(5,5,figsize=(25,15))
+    # #cmap = mpl.cm.jet(vmin=0, vmax=1)
+    # #norma = mpl.colors.Normalize(vmin=0, vmax=1)
+    # norm=plt.Normalize(0,2)
+    # for ii in range(5):
+    #     for jj in range(5):
+    #         if(ii==jj):
+    #             ax[ii,jj].hist(df_params[legendounet[ii]], weights=np.zeros_like(df_params[legendounet[ii]]) + 1. / df_params[legendounet[ii]].size)
+    #             ax[ii,jj].set_xlabel(legendounet[ii],fontsize=fontouney)
+    #             ax[ii,jj].set_ylabel('Rel Freq.',fontsize=fontouney)
+    #             ax[ii,jj].tick_params(axis='both', which='major', labelsize=fontouney)
+    #             ax[ii,jj].grid() 
+                
+    #         else:
+    #             sc=ax[ii,jj].scatter(df_params[legendounet[ii]],df_params[legendounet[jj]],c=df_params.RMSE,cmap = 'jet',norm=norm)
+    #             #plt.colorbar(sc,ax=ax[ii,jj])
+    #             ax[ii,jj].grid()
+    #             ax[ii,jj].set_xlabel(legendounet[ii],fontsize=fontouney)
+    #             ax[ii,jj].set_ylabel(legendounet[jj],fontsize=fontouney)
+    #             ax[ii,jj].tick_params(axis='both', which='major', labelsize=fontouney)
+
+     
+    
+    # left  = 0.045  # the left side of the subplots of the figure
+    # right = 0.988    # the right side of the subplots of the figure
+    # bottom = 0.049   # the bottom of the subplots of the figure
+    # top = 0.987      # the top of the subplots of the figure
+    # wspace = 0.224   # the amount of width reserved for blank space between subplots
+    # hspace = 0.290   # the amount of height reserved for white space between subplots
+    # plt.subplots_adjust(left=left, bottom=bottom, right=right, top=top, wspace=wspace, hspace=hspace)
+    
+    # f1.savefig('./plots/RMSEVOLANDTWT_'+ouca+'-'+Nama+'.png',format='png')
+    
+####################    
+################### histogram
     df_params_sorted=df_params.sort_values(by=['RMSE'],inplace=False)
     df_params_sorted.reset_index(drop=True, inplace=True)
     pc=0.1#10percent
     
     df_params_sorted_cut=df_params_sorted[0:np.int(np.round(pc*len(df_params_sorted)))]
     df_params_sorted_cut.reset_index(drop=True, inplace=True)
-    
+
     #df_params=df_params[(df_params['RMSE']0.03) & (df_params['Ks']<0.49) & (df_params['Ks']>0.07) & (df_params['n']<10.1) ]
     plt.close('all')
     legendounet=['ts','n','alpha','Ks']
-    (f2, ax)= plt.subplots(2,2,figsize=(25,15))
+    (f2, ax)= plt.subplots(2,3,figsize=(25,15))
     kk=0
-    fontouney=20
+    
     for ii in range(2):
         for jj in range(2):
             ax[ii,jj].hist(df_params_sorted_cut[legendounet[kk]], weights=np.zeros_like(df_params_sorted_cut[legendounet[kk]]) + 1. / df_params_sorted_cut[legendounet[kk]].size)
@@ -221,9 +270,19 @@ for filit,filiv in zip(hahat,hahav):
             ax[ii,jj].grid() 
             ax[ii,jj].set_ylim([0, 0.3])
             kk=kk+1
+    
+    cmap = mpl.cm.jet((df_params_sorted_cut['RMSE'].values - df_params_sorted_cut['RMSE'].min())/\
+    (df_params_sorted_cut['RMSE'].max() - df_params_sorted_cut['RMSE'].min()))    
+    for  color, (index, row) in zip(cmap,df_params_sorted_cut.iterrows()):
+        ax[0,2].plot(Time_TWT_XP, row['TWT'], c=color)
+        ax[1,2].plot(Time_TWT_XP, row['VOL'], c=color)
+        ax[0,2].grid()
+        ax[1,2].grid()
+    ax[0,2].plot(Time_TWT_XP, TWT_XP, marker = '*', markersize = 10, mfc = 'k')
+    ax[1,2].plot(Time_TWT_XP, VOL_XP, marker = '*', markersize = 10, mfc = 'k')    
     f2.suptitle(str(100*pc)+'% du meilleur modele '+ ouca+'-'+Nama, fontsize=fontouney)
     f2.savefig('./plots/Histo_'+str(100*pc)+'pc_'+ouca+'-'+Nama+'.png',format='png')
-
+###################
 
 
 
