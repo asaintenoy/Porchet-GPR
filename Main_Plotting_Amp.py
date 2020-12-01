25f2f87bdc198b5a3aabf7c06b67e721ea31ab89
eleger <emmanuel.leger@u-psud.fr>
2020-12-01 19:47:09 +0100

    ajout Amp

--- Main_Plotting_Amp.py
+++ Main_Plotting_Amp.py
@@ -42,7 +42,8 @@ else:
     os.chdir(X_path)
     
     
-    
+Time_TWT_XP = np.array([0, 0.25, 0.50, 0.75, 1.00, 1.50, 2.00, 2.50, 3.00, 4.00, 5.00, 6.00])    
+ 
 #%%
 foldernama='./OUT_20201121dtrou30_rtrou3_tr5.0/'
 fname=next(os.walk(foldernama))[1] 
@@ -60,7 +61,7 @@ for ii in fname:
     paramMVG = ParamMVG(tr=p[2], ts=p[0], ti=p[1], Ks=p[5], n=p[3], alpha=p[4])
     try:
         temp=F_extractTWT(foldernama+ii)
-        Amp=np.genfromtxt(foldernama+ii+'/Amp_EL.csv',delimiter=',',skip_header=1)
+        Amp=np.genfromtxt(foldernama+ii+'/AMP_EL.csv',skip_header=1)
         ##
         bibi = 0
         #rmseTwt=np.sqrt(np.mean((temp-TWT_XP)**2)/len(TWT_XP))/(max(TWT_XP)-min(TWT_XP))
@@ -70,19 +71,32 @@ for ii in fname:
         #rmse=np.sqrt(rmseTwt**2+rmsevol**2)
     except:
         bibi=1
-        Amp=np.nan
+        Amp = np.empty(np.size(Time_TWT_XP))
+        Amp[:] = np.nan
+        #Amp=np.nan
         #rmseTwt=np.nan
         #rmsevol=np.nan
         #rmse=np.nan
     
-    lst.append([paramMVG.tr,paramMVG.ts,paramMVG.ti,paramMVG.n,paramMVG.alpha,paramMVG.Ks,bibi,Amp])
+    lst.append([paramMVG.tr,paramMVG.ts,paramMVG.ti,paramMVG.n,paramMVG.alpha,paramMVG.Ks,bibi,np.abs(Amp)])
     
 df_params=pd.DataFrame(lst,columns=['tr','ts','ti','n','alpha','Ks','Converged','Amp']) 
 
 df_params['alpha']=df_params['alpha']*100
-df_params['VOL']=df_params['VOL']*0.001
+#df_params['VOL']=df_params['VOL']*0.001
 
-#%%
+#%% plotting
+#cmap = mpl.cm.jet(len(df_params))
+gni=df_params.sample(10000)
+cmap = mpl.cm.jet(len(gni))
+(f2, ax)= plt.subplots(1,1,figsize=(25,15))
+for  color, (index, row) in zip(cmap,gni.iterrows()):
+    ax.plot(Time_TWT_XP, row['Amp'])
+    #ax[1,2].plot(Time_TWT_XP, row['VOL'], c=color)
+ax.grid()
 
+ax.set_xlabel('Exp. Time (min)',fontsize=fontouney)
+ax.set_ylabel('Amp ',fontsize=fontouney)
+#ax[1,2].set_xlabel('Exp. Time (min)',fontsize=fontouney)
 
 
